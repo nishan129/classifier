cbdb0152402cbe39b4dfc80655a7f785fc7199fe
nishant129 <nishantborkar139@gmail.com>
2024-04-09 16:45:13 +0530

    All file is created

---
+++ tamplate.py
@@ -0,0 +1,45 @@
+import os
+from pathlib import Path
+
+
+project_name = "classifier"
+file = [
+    ".github/workflows/.gitkeep",
+     f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/utils/common.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/entity/config_entity.py",
+    f"src/{project_name}/constant/__init__.py",
+    "config/config.yaml",
+    "dvc.yaml",
+    "params.yaml",
+    "schema.yaml",
+    "main.py",
+    "app.py",
+    "Dockerfile",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb",
+    "tamplates/index.html"
+]
+
+for filepath in file:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+    
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        
+        
+    if not(os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath,'w') as f:
+            pass
+    
+    
+    else: 
+        print("error")
