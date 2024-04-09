4a2e3864ef2bda394a90ccaf04bff66c5141dc52
nishant129 <nishantborkar139@gmail.com>
2024-04-09 20:29:24 +0530

    All setup is done

---
+++ main.py
@@ -0,0 +1,4 @@
+from src.classifier.logger import logging
+if __name__ == '__main__':
+    print("ok")
+    logging.info(f"Welcome to the project")
---
+++ src/classifier/utils/common.py
@@ -0,0 +1,102 @@
+import os
+from ensure import ensure_annotations
+import yaml
+from box import ConfigBox
+from pathlib import Path
+from typing import Any
+from src.classifier.exception import ModelException
+from src.classifier.logger import logging
+import json, sys
+import joblib
+
+
+@ensure_annotations
+def read_yaml(filepath:Path) -> ConfigBox:
+    """read yaml file and return
+
+    Args:
+        filepath (str): path like input
+    
+    Raises:
+        ValueError: if yaml file is empty
+        e: empty file
+
+    Returns:
+        ConfigBox: config box type
+    """
+    try:
+        with open(filepath, 'r') as file:
+            data = yaml.safe_load(file)
+            return ConfigBox(data)
+    except Exception as e:
+        raise ModelException(e,sys)
+    
+@ensure_annotations
+def write_yaml(filepath:Path, data:Any) -> ConfigBox:
+    """Write yaml file and return config box
+
+    Args:
+        filepath (Path): To save in yaml file path
+        data (Any): data to save in yaml file
+
+    Returns:
+        ConfigBox: writern file path
+    """
+    try:
+        os.makedirs(os.path.dirname(filepath),exist_ok=True)
+        with open(filepath, 'w') as file:
+            yaml.dump(data,file,default_flow_style=False)
+    except Exception as e:
+        raise ModelException(e,sys)
+    
+@ensure_annotations   
+def create_directory(path_to_directory:list, verbose=True):
+    """create list of directories
+
+    Args:
+        path_to_directory (list): list of path of directory
+        verbose (bool, optional): Ingnoring if multiple directory is created Defaults to True.
+    """
+    try:
+        for path in path_to_directory:
+            os.makedirs(path,exist_ok=True)
+            if verbose: 
+                logging.info(f"Create directory {path}")
+    except Exception as e:
+        raise ModelException(e,sys)
+
+@ensure_annotations
+def save_json(path:str, data:dict):
+    """Save Dict to JSON file
+
+    Args:
+        path (str): path to save dictionary in to json
+        data (dict): dictionary to save
+
+    Raises:
+        ModelException: error
+    """
+    try:
+        with open(path, 'w') as file:
+            json.dump(data,file, indent=4)
+            logging.info(f"Save json file {file}")
+    except Exception as e:
+        raise ModelException(e,sys)
+    
+    
+@ensure_annotations
+def load_json(path:str) -> ConfigBox:
+    """To load a json file
+
+    Args:
+        path (str): file path there have json files
+
+    Returns:
+        ConfigBox: to file path
+    """
+    try:
+        with open(path, 'r') as file:
+            data = json.load(file)
+            return ConfigBox(data)
+    except Exception as e:
+        raise ModelException(e,sys)
